7d5e8ad62a3d2322ad43f12b6279394a43d48a3e
alhakim2709 <54828883+alhakim2709@users.noreply.github.com>
2021-06-10 22:08:14 +0700

    fixing product hover bug

--- Project/HTML File/JS Files/GalleryJS.js
+++ Project/HTML File/JS Files/GalleryJS.js
@@ -1,33 +1,3 @@
-const left = $("#prev_arrow")
-const right = $("#next_arrow")
-const images = $(".row > div")
-const firstImage = images.first()
-const slideImages = $(".column")
-
-let position = 0;
-
-left.click(() => {
-    position--;
-    if(position < 0){
-        position = images.lenght - 1;
-    }
-    updateSlider();
-});
-right.click(() => {
-    position++;
-    if(position >= images.lenght){
-        position = 0;
-    }
-    updateSlider();
-})
-
-function updateSlider(){
-    images.first().animate({
-        marginLeft: position * firstImage.width() * -1
-    })
-}
-
-
 function showImage(imgs){
     var expandImg = document.getElementById("expandedImg");
     var imgText = document.getElementById("imgtext")
