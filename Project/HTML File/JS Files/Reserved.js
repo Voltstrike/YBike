011469137ea68d089c02a7800123f08da031a862
alhakim2709 <54828883+alhakim2709@users.noreply.github.com>
2021-06-15 21:47:57 +0700

    adding selection js in reserve

---
+++ Project/HTML File/JS Files/Reserved.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $("#Brand-selection").change(function () {
+        var val = $(this).val();
+        if (val == "Kawasako") {
+            $("#Type").html("<option value='Kawasako1'>Kawasako Ninja ZX-6R</option><option value='Kawasko2'>Kawasak0 Ninja H2</option><option value='Kawasko3'>Kawasako Z1000</option>");
+        } else if (val == "Vixian") {
+            $("#Type").html("<option value='Vixian1'>Vixian R</option><option value='Vixian2'>Vixian Live The Legend</option><option value='Vixian3'>Vixian Monster Energy MotoGP Edition</option>");
+        } else if (val == "Default") {
+            $("#Type").html("<option value=''>Select a Brand</option>");
+        }
+    });
+});
